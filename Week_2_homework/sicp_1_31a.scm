8c21629f11a20cfd8a52d1644a8276609952662e
K <gemigpost@hotmail.com>
2021-06-03 13:23:54 +0200

    week 2 homework question 1

---
+++ Week_2_homework/sicp_1_31a.scm
@@ -0,0 +1,27 @@
+#| Exercise 1.31 a.  The sum procedure is only the simplest of a vast number of similar abstractions
+that can be captured as higher-order procedures.
+Write an analogous procedure called product that returns the product of 
+the values of a function at points over a given range. 
+Show how to define factorial in terms of product. 
+Also use product to compute approximations to π using the formula
+π / 4 = 2 ⋅ 4 ⋅ 4 ⋅ 6 ⋅ 6 ⋅ 8 ⋅ ⋯
+        3 ⋅ 3 ⋅ 5 ⋅ 5 ⋅ 7 ⋅ 7 ⋅ ⋯ 
+ |#
+
+(define (sumEven a s e)
+(if (>= s e)
+a
+(if (= (remainder s 2) 0)
+  (* a (sumEven (+ a 2) (+ s 1) e))
+  (* a (sumEven a (+ s 1) e)))))
+
+
+(define (sumOdd a s e)
+(if (>= s e)
+a
+(if (= (remainder s 2) 0)
+  (* a (sumOdd a (+ s 1) e))
+  (* a (sumOdd (+ a 2) (+ s 1) e)))))
+
+(define (product start steps) (* 4 (/ (sumeven start 0 steps) (sumodd (+ start 1) 0 steps))))
+; (product 2 155) is around what it can handle
